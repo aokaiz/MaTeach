75f209eff1266a67b1a398ed3935d6f877baba4b
aokaiz <aokaiz@163.com>
2026-04-29 07:13:29 +0800

    Update: eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,14 @@ module.exports = {
   apps: [{
     name: 'Mateach',
     script: 'node_modules/.bin/next',
-    args: 'start -p 3000',
+    args: 'start -p 3001',
     cwd: '/var/www/MaTeach',
     instances: 1,
     autorestart: true,
     watch: false,
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
-      DB_HOST: 'localhost',
-      DB_PORT: 5432,
-      DB_USER: 'hellouser',
-      DB_PASSWORD: '436',
-      DB_NAME: 'hellodb',
+      PORT: 3001,
     }
   }]
 }
